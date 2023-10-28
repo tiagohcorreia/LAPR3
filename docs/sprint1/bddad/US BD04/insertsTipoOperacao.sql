37aa9d7754e8a1b265401fc378215b2f9578945b
Rafael Rocha <1201260@isep.ipp.pt>
2023-10-28 22:28:37 +0100

    update inserts operacoes e tipo operacao

--- docs/sprint1/bddad/US BD04/insertsTipoOperacao.sql
+++ docs/sprint1/bddad/US BD04/insertsTipoOperacao.sql
@@ -1,8 +1,8 @@
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (101, 'Plantacao', 'un');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (102, 'Rega', 'm3');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (103, 'Fertilizacao', 'kg');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (104, 'Poda', 'un');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (105, 'Aplicacao fitofarmaco', 'kg');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (106, 'Sementeira', 'kg');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (107, 'Colheita', 'kg');
-INSERT INTO Tipo_Operacao(ID, Nome, Unidade) values (108, 'Incorporacao no solo', 'ha');
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (101, Plantação, un);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (102, Rega, m3);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (103, Fertilização, kg);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (104, Poda, un);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (105, Aplicação fitofármaco, kg);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (106, Sementeira, kg);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (107, Colheita, kg);		
+INSERT INTO Tipo_Operacao(id,nome,unidade) values (108, Incorporação no solo, ha);		
