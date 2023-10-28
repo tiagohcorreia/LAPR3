a5cb3afb18284f0f03f4229854655b70a310a545
Rafael Rocha <1201260@isep.ipp.pt>
2023-10-28 22:34:45 +0100

    inserts operacao

--- docs/sprint1/bddad/US BD04/insertsTipoOperacao.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (101, Plantação, un);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (102, Rega, m3);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (103, Fertilização, kg);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (104, Poda, un);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (105, Aplicação fitofármaco, kg);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (106, Sementeira, kg);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (107, Colheita, kg);		
-INSERT INTO Tipo_Operacao(id,nome,unidade) values (108, Incorporação no solo, ha);		
