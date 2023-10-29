0b9d7304570f19cbf59356afaa10c5337c8c1715
1221638 <1221638@isep.ipp.pt>
2023-10-29 22:46:15 +0000

    - implementaçao [bddad][us03]

--- docs/sprint1/bddad/US BD06/us_06.sql
+++ docs/sprint1/bddad/US BD06/us_06.sql
@@ -15,3 +15,27 @@ WHERE
         op.data >= '2016-10-06' AND op.data <= '2016-10-10'
 GROUP BY
     p.id, fp.tipo_id;
+
+
+
+SELECT
+    parcela_id,
+    COUNT(*) AS num_fatores_de_producao
+FROM
+    Plantacao p
+WHERE
+        p.parcela_id IS NOT NULL
+        AND EXISTS (
+        SELECT 1
+        FROM Plantacao_Temporaria pt
+        WHERE pt.plantacao_id = p.id
+          AND TO_DATE(TO_CHAR(pt.data_fim, 'YYYY/MM/DD'), 'YYYY/MM/DD') >= TO_DATE('2022/01/01', 'YYYY/MM/DD') -- Data de início do intervalo
+          AND TO_DATE(TO_CHAR(pt.data_fim, 'YYYY/MM/DD'), 'YYYY/MM/DD') <= TO_DATE('2022/12/31', 'YYYY/MM/DD') -- Data de fim do intervalo
+    )
+   OR EXISTS (
+    SELECT 1
+    FROM Plantacao_Permanente pp
+    WHERE pp.plantacao_id = p.id
+)
+GROUP BY
+    parcela_id;
