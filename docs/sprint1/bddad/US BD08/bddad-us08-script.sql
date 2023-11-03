246a188fea52116cccd1283c12cd52da5b9fdf51
Dephane <1221636@isep.ipp.pt>
2023-11-03 15:37:21 +0000

    finished script for us08

--- docs/sprint1/bddad/US BD08/bddad-us08-script.sql
+++ docs/sprint1/bddad/US BD08/bddad-us08-script.sql
@@ -9,11 +9,10 @@ ORDER BY COUNT(afp.operacao_id) DESC
 SELECT afp.fator_producao_id, COUNT(afp.operacao_id) AS num_aplicacoes
 FROM Aplicacao_FP afp
          JOIN Operacao o ON afp.operacao_id = o.id
-WHERE o.data BETWEEN TO_DATE('2020-07-03', 'YYYY-MM-DD') AND TO_DATE('2019-10-04', 'YYYY-MM-DD')
+WHERE o.data BETWEEN TO_DATE('2019-07-03', 'YYYY-MM-DD') AND TO_DATE('2020-10-04', 'YYYY-MM-DD')
 GROUP BY afp.fator_producao_id
 ORDER BY COUNT(afp.operacao_id) DESC
     FETCH FIRST 1 ROW ONLY;
-
 
 
 SELECT afp.fator_producao_id, COUNT(afp.operacao_id) AS num_aplicacoes
