92dbcb3f5d4fcd3b23a07b54967f9f91591f70f3
1221638 <1221638@isep.ipp.pt>
2023-11-18 21:09:33 +0000

    L3IP-44 #time 1h #melhorias no procedimento pl/sql para registrar podas

--- docs/sprint2/bddad/US BD15/bddad-us-15.sql
+++ docs/sprint2/bddad/US BD15/bddad-us-15.sql
@@ -6,19 +6,30 @@ CREATE OR REPLACE PROCEDURE registrarPoda(parcelaId Parcela.id%TYPE NOT NULL, va
     IS
     id Operacao_Agricola.id%TYPE := (SELECT MAX(id)
                                      FROM Operacao_Agricola);
-    id_exists_excep EXCEPTION;
+    id_operacao_existe EXCEPTION;
+    parcela_nao_existe EXCEPTION;
+    variedade_nao_existe EXCEPTION;
 BEGIN
     id := id + 1;
-    IF (
-        id IN (SELECT id FROM Operacao_Agricola)
-        ) THEN
-        RAISE id_exists_excep;
+
+    IF (id IN Operacao_Agricola.id) THEN
+        RAISE id_operacao_existe;
+    ELSIF (parcelaId NOT IN Parcela.id) THEN
+        RAISE parcela_nao_existe;
+    ELSIF (variedadeId NOT IN Variedade.id) THEN
+        RAISE variedade_nao_existe;
     END IF;
+
     INSERT INTO Operacao_Agricola(id, data)
     VALUES (id, data);
     INSERT INTO Poda(operacao_id, parcela_id, variedade_id, quantidade, metodo_execucao_id)
     VALUES (id, parcelaId, variedadeId, quantidade, metodoExecucaoId);
+
 EXCEPTION
-    WHEN id_exists_excep
+    WHEN id_operacao_existe
         THEN DBSM_OUTPUT.put_line('Não foi possível registrar a operação.');
+    WHEN parcela_nao_existe
+        THEN DBSM_OUTPUT.put_line('A parcela especificada não está registada na base de dados');
+    WHEN variedade_nao_existe
+        THEN DBSM_OUTPUT.put_line('A variedade especificada não está registada na base de dados');
 END;
