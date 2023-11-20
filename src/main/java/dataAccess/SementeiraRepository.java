47aef09c99a398ccc245cd71fbcbdfc431dece14
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-11-20 00:33:43 +0000

    L3IP-55 #time 3h # Classes base para us lp4 adicionados

---
+++ src/main/java/dataAccess/SementeiraRepository.java
@@ -0,0 +1,66 @@
+package dataAccess;
+
+import domain.Sementeira;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SementeiraRepository {
+
+    public SementeiraRepository() {
+    }
+
+    public void sementeiraRegister(int operacaoId, int parcelaId, int variedadeId, float quantidade, float area, int metodoExecucaoId) throws SQLException {
+
+        CallableStatement callableStatement = null;
+
+        try {
+
+            Connection connection = DatabaseConnection.getInstance().getConnection();
+            callableStatement = connection.prepareCall("{ call sementeiraRegister(?, ?, ?, ?, ?, ?)}");
+
+            callableStatement.setInt(1, operacaoId);
+            callableStatement.setInt(2, parcelaId);
+            callableStatement.setInt(3, variedadeId);
+            callableStatement.setFloat(4, quantidade);
+            callableStatement.setFloat(5, area);
+            callableStatement.setInt(6, metodoExecucaoId);
+
+            callableStatement.execute();
+            connection.commit();
+
+        } finally {
+
+            if (!Objects.isNull(callableStatement)) {
+
+                callableStatement.close();
+            }
+        }
+    }
+
+    private List<Sementeira> resultSetToList(ResultSet resultSet) throws SQLException {
+
+        List<Sementeira> sementeiras = new ArrayList<>();
+
+        while (true) {
+
+            if (!resultSet.next()) break;
+
+            Sementeira sementeira = new Sementeira(
+                    resultSet.getInt("operacaoId"),
+                    resultSet.getInt("parcelaId"),
+                    resultSet.getInt("veriedadeId"),
+                    resultSet.getFloat("quantidade"),
+                    resultSet.getFloat("area"),
+                    resultSet.getInt("metodoExecucaoId")
+            );
+            sementeiras.add(sementeira);
+        }
+        return sementeiras;
+    }
+}
--- src/main/java/ui/user/ProductOwnerUI.java
+++ src/main/java/ui/user/ProductOwnerUI.java
@@ -1,10 +1,35 @@
 package ui.user;
 
+import ui.funcionalidades.SementeiraRegisterUI;
+import ui.menu.MenuItem;
+import ui.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductOwnerUI implements Runnable {
 
+    public ProductOwnerUI() {
+
+    }
 
     @Override
     public void run() {
 
+        List<MenuItem> options = new ArrayList<MenuItem>();
+
+        options.add(new MenuItem("Registar Sementeira", new SementeiraRegisterUI()));
+
+        int option = 0;
+
+        do {
+            option = Utils.showAndSelectIndex(options, "\n\nMain Menu");
+
+            if ((option >= 0) && (option < options.size())) {
+                options.get(option).run();
+            }
+
+        } while (option != -1);
     }
+
 }
