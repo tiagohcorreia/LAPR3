3a240c2ac5f88038a48e721d81caeadd1d84f251
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-11-23 17:14:37 +0000

    Ajustes nas UI

--- src/main/java/ui/menu/MainMenuUI.java
+++ src/main/java/ui/menu/MainMenuUI.java
@@ -6,26 +6,29 @@ import ui.funcionalidades.EstadoRegaUI;
 import ui.funcionalidades.SimularSistemaRegaUI;
 import ui.utils.ChooseRoleUI;
 import ui.utils.Utils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainMenuUI implements Runnable {
 
-    public MainMenuUI(){
+    public MainMenuUI() {
     }
 
     @Override
     public void run() {
 
         List<MenuItem> options = new ArrayList<MenuItem>();
-        options.add(new MenuItem("Teste conexao com base de dados", new DatabaseConnectionTestUI()));
 
+        options.add(new MenuItem("Teste conexao com base de dados", new DatabaseConnectionTestUI()));
         options.add(new MenuItem("Database Connection Test", new DatabaseConnectionTestUI()));
         options.add(new MenuItem("Gerar Plano de Rega", new SimularSistemaRegaUI()));
         options.add(new MenuItem("Verificar Estado da Rega", new EstadoRegaUI()));
         options.add(new MenuItem("Escolher User", new ChooseRoleUI()));
         options.add(new MenuItem("Exit", new ExitUI()));
+
         int option = 0;
+
         do {
             option = Utils.showAndSelectIndex(options, "\n\nMenu principal");
 
