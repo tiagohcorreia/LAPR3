6e4979fed3e34f8f24bcc09d5cf39a5088790e1e
Tiago Correia <106349052+tiagohcorreia@users.noreply.github.com>
2023-11-24 14:17:08 +0000

    L3IP-55 #time 2h # Funcionalidades para listagens de Variedades, Parecelas e Metodos de Execução adicionado

--- docs/sprint2/bddad/US BD11/us11-script.sql
+++ docs/sprint2/bddad/US BD11/us11-script.sql
@@ -1,33 +1,63 @@
-CREATE OR REPLACE PROCEDURE registarSementeira(parcelaId Parcela.id TYPE NOT NULL, variedadeId Vaviedade, variedadeTempId Variedade_Temporaria
-       quantidade Sementeira.quantidade% TYPE NOT NULL, area Sementeira.area% TYPE NOT NULL,
-       metodo_execucao_id Metodo_Execucao.id)
-
-    IS
-        id Operacao_Agricola.id% TYPE := (SELECT MAX(id) FROM Operacao_Agricola)
-        id_operacao_existe EXCEPTION;
-        parcela_nao_existe EXCEPTION;
-        metodo_nao_existe EXCEPTION;
-        variedade_nao_existe EXCEPTION;
+CREATE OR REPLACE PROCEDURE registarSementeira(
+    parcelaId Parcela.id% TYPE := NULL,
+    variedadeId Variedade.id% TYPE,
+    variedadeTempId Variedade_Temporaria.id% TYPE,
+    quantidade Sementeira.quantidade% TYPE NOT NULL,
+    area Sementeira.area% TYPE NOT NULL,
+    metodo_execucao_id Metodo_Execucao.id% TYPE
+)
+IS
+    id Operacao_Agricola.id% TYPE := (SELECT MAX(id) + 1 FROM Operacao_Agricola);
 BEGIN
+    -- Verificar se parcela_id existe
+    SELECT COUNT(*)
+    INTO id
+    FROM Parcela
+    WHERE id = parcelaId;
 
-    id := id + 1
+IF
+id = 0 THEN
+        -- Caso a Parcela não exista
+        DBMS_OUTPUT.put_line('Parcela especificada não existe na base de dados.');
+        RETURN -1;
+        END IF;
 
-    IF (id IN Operacao_Agricola.id) THEN RAISE id_operacao_existe
+        -- Verificar se metodo de execução existe
+        SELECT COUNT(*)
+        INTO id
+        FROM Metodo_Execucao
+        WHERE id = metodo_execucao_id;
 
-    ELSIF (parcelaId NOT IN Parcela.id) THEN RAISE parcela_nao_existe
+IF
+id = 0 THEN
+        -- Caso metodo de execução não existe
+        DBMS_OUTPUT.put_line('Método de execução especificado não existe na base de dados.');
+        RETURN -1;
+        END IF;
 
-    ELSIF (metodoExecucaoId NOT IN Metodo_Execucao.id) THEN RAISE metodo_nao_existe
+-- Verificar se variedade existe
+IF
+variedadeId IS NOT NULL THEN
+        SELECT COUNT(*)
+        INTO id
+        FROM Variedade
+        WHERE id = variedadeId;
 
-    ELSIF (variedadeId NOT IN Variedade.id) THEN RAISE variedade_nao_existe
-    END IF;
+IF
+id = 0 THEN
+            -- Caso variedade não exista
+            DBMS_OUTPUT.put_line('Variedade especificada não existe na base de dados.');
+            RETURN -1;
+            END IF;
+            END IF;
 
-    INSERT INTO Operacao_Agricola(id) VALUES (id)
-    INSERT INTO Sementeira(operacao_id, parcela_id, variedade_id, quantidade, area, metodo_execucao_id) VALUES (id, parcelaID, variedadeId, quantidade, area, metodoExecucaoId);
+-- Insert into Operacao_Agricola
+INSERT INTO Operacao_Agricola(id) VALUES (id);
 
-EXCEPTION
+-- Insert into Sementeira
+INSERT INTO Sementeira(operacao_id, parcela_id, variedade_id, quantidade, area, metodo_execucao_id)
+VALUES (id, parcelaId, variedadeId, quantidade, area, metodo_execucao_id);
 
-    WHEN id_operacao_nao_existe THEN DBSM_OUTPUT.put_line('Não foi possível registar a sementeira.');
-    WHEN parcela_nao_existe THEN DBSM_OUTPUT.put_line('Parcela especificada não existe na base de dados.');
-    WHEN veriedade_nao_existe THEN DBSM_OUTPUT.put_line('Variedade especificada não existe na base de dados.');
-    WHEN metodo_nao_existe THEN DBSM_OUTPUT.put_line('Método de execução especificado não existe na base de dados.');
+DBMS_OUTPUT.put_line('Sementeira registrada com sucesso.');
+RETURN 1;
 END;
