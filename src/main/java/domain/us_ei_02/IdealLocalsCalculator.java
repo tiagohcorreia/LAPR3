fa0fdce025ad6f99ab0b19ca9292f00b80f8c65c
1221638 <1221638@isep.ipp.pt>
2023-11-24 10:00:25 +0000

    L3IP-88 #time 45m #[esinf][us02][implementaçao]

--- src/main/java/domain/us_ei_02/IdealLocalsCalculator.java
+++
@@ -1,4 +0,0 @@
-package domain.us_ei_02;
-
-public class IdealLocalsCalculator {
-}
---
+++ src/main/java/domain/us_ei_02/IdealVerticesCalculator.java
@@ -0,0 +1,69 @@
+package domain.us_ei_02;
+
+import esinf.Algorithms;
+import esinf.Graph;
+
+import java.util.*;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+
+public class IdealVerticesCalculator<V, E> {
+
+    private Graph<V,E> graph;
+    private Comparator<E> comparator;
+    private BinaryOperator<E> binaryOperator;
+    private E neutralValue;
+
+    public IdealVerticesCalculator(Graph<V,E> graph, Comparator<E> comparator, BinaryOperator<E> binaryOperator, E neutralValue){
+        this.graph=graph;
+        this.comparator=comparator;
+        this.binaryOperator=binaryOperator;
+        this.neutralValue=neutralValue;
+    }
+
+    public Map<V, Integer> getVerticesCentrality(){
+
+        Map<V, ArrayList<LinkedList<V>>> allShortestPathsForAllVertices=getAllShortestPathsForAllVertices();
+        return getVerticesCentrality(allShortestPathsForAllVertices);
+    }
+
+    public  Map<V, ArrayList<LinkedList<V>>> getAllShortestPathsForAllVertices(){
+
+        Map<V, ArrayList<LinkedList<V>>> allShortestPathsForAllVertices=new HashMap<>();
+
+        try{
+            for (V v: graph.vertices()){
+
+                ArrayList<LinkedList<V>> paths=new ArrayList<>();
+                ArrayList<E> distances=new ArrayList<>();
+                boolean success=Algorithms.shortestPaths(graph, v, comparator, binaryOperator, neutralValue, paths, distances);
+
+                if (!success){
+                    throw new Exception("Não foi possível obter os caminhos para todos o vértices");
+                } else allShortestPathsForAllVertices.put(v, paths);
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return allShortestPathsForAllVertices;
+    }
+
+    private Map<V, Integer> getVerticesCentrality(Map<V, ArrayList<LinkedList<V>>> allShortestPathsForAllVertices){
+        Map<V, Integer> verticesCentrality=new HashMap<>();
+
+        for (V v: graph.vertices()){
+            //counts how many shortests paths the vertice belongs to
+            int count=0;
+            for (LinkedList<V> l: allShortestPathsForAllVertices.get(v)){
+                if (l.contains(v)){
+                    count++;
+                }
+            }
+            verticesCentrality.put(v,count);
+        }
+        return verticesCentrality;
+    }
+}
