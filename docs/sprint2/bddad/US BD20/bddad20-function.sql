a771b968c5f3659eae48e3b7e9af1477fcd54654
Luísa Veríssimo <1212124@isep.ipp.pt>
2023-11-26 20:06:55 +0000

    Resolução da US 20 base de dados

--- docs/sprint2/bddad/US BD20/bddad20-function.sql
+++ docs/sprint2/bddad/US BD20/bddad20-function.sql
@@ -1,17 +1,10 @@
-SELECT
-    p.id AS parcela_id,
-    EXTRACT(MONTH FROM TO_DATE(o.data, 'DD-MM-YYYY')) AS month,
-    EXTRACT(YEAR FROM TO_DATE(o.data, 'DD-MM-YYYY')) AS year,
-    SUM(r.duracao) AS total_rega
-FROM
-    Parcela p
-        JOIN
-    Rega r ON p.id = r.parcela_id
-        JOIN
-    Operacao_Agricola o ON r.operacao_id = o.id
-WHERE
-    TO_DATE(o.data, 'DD-MM-YYYY') BETWEEN TO_DATE('start_date', 'DD-MM-YYYY') AND TO_DATE('end_date', 'DD-MM-YYYY')
-GROUP BY
-    p.id, EXTRACT(MONTH FROM TO_DATE(o.data, 'DD-MM-YYYY')), EXTRACT(YEAR FROM TO_DATE(o.data, 'DD-MM-YYYY'))
-ORDER BY
-    p.id, EXTRACT(YEAR FROM TO_DATE(o.data, 'DD-MM-YYYY')), EXTRACT(MONTH FROM TO_DATE(o.data, 'DD-MM-YYYY'));
+
+SELECT EXTRACT(MONTH FROM op_agr.data) as mes, sum(rg.duracao) as totais_rega
+FROM rega rg, operacao_agricola op_agr, setor_rega str, plantacao plt, setorrega_plantacao str_plt
+WHERE 108 = plt.parcela_id
+  AND plt.id = str_plt.plantacao_id
+  AND str.id = str_plt.setor_id
+  AND str.id = rg.setor_id
+  AND rg.operacao_id = op_agr.id
+  AND op_agr.data BETWEEN TO_DATE('01-06-2023', 'dd-mm-yyyy') and TO_DATE('06-11-2023', 'dd-mm-yyyy')
+GROUP BY EXTRACT(MONTH FROM op_agr.data);
