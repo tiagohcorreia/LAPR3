89d08dc72cc33b587faeb77097e489ed8cc5f3a1
1221638 <1221638@isep.ipp.pt>
2023-12-05 12:20:19 +0000

    ...

--- docs/sprint2/bddad/US BD20/bddad20-function.sql
+++ docs/sprint2/bddad/US BD20/bddad20-function.sql
@@ -1,37 +1,53 @@
-create or replace procedure getMonthlyWateringTotal(parcelaId parcela.id%type,
-    beginDate operacao_agricola.data%type,
-    endDate operacao_agricola.data%type)
-is
-    cursor c1 is SELECT EXTRACT(MONTH FROM op_agr.data) as mes, sum(rg.duracao) as totais_rega
-    FROM rega rg, operacao_agricola op_agr, setor_rega str, plantacao plt, setorrega_plantacao str_plt
-    WHERE parcelaId = plt.parcela_id
-    AND plt.id = str_plt.plantacao_id
-    AND str.id = str_plt.setor_id
-    AND str.id = rg.setor_id
-    AND rg.operacao_id = op_agr.id
-    AND op_agr.data BETWEEN beginDate and endDate
-    GROUP BY EXTRACT(MONTH FROM op_agr.data);
-
-	mes number(2);
-	totalRega number(10);
-	parcelaNome parcela.nome%type;
+create or replace function getMonthlyWateringTotal(parcelaId parcela.id%type,
+                                                   beginDate operacao_agricola.data%type,
+                                                   endDate operacao_agricola.data%type)
+    return sys_refcursor
+    is
+    return sys_refcursor;
 begin
-	select nome into parcelaNome
-    from parcela
-    where parcela.id=parcelaId;
-	dbms_output.put_line('Parcela: ' || parcelaNome);
-	dbms_output.put_line('-----------------------------');
-	open c1;
-    loop
-    fetch c1 into mes, totalRega;
-	dbms_output.put_line('Mes: ' || mes);
-	dbms_output.put_line('Total de rega: ' || mes);
-	dbms_output.put_line('-----------------------------');
-	exit when c1%notfound;
-	end loop;
-	close c1;
+    open return for
+        SELECT EXTRACT(MONTH FROM op_agr.data) as mes, sum(rg.duracao) as totais_rega
+        FROM rega rg,
+             operacao_agricola op_agr,
+             setor_rega str,
+             plantacao plt,
+             setorrega_plantacao str_plt
+        WHERE parcelaId = plt.parcela_id
+          AND plt.id = str_plt.plantacao_id
+          AND str.id = str_plt.setor_id
+          AND str.id = rg.setor_id
+          AND rg.operacao_id = op_agr.id
+          AND op_agr.data BETWEEN beginDate and endDate
+        GROUP BY EXTRACT(MONTH FROM op_agr.data);
+    return return;
 end;
 
-begin 
-    getMonthlyWateringTotal(108, to_date('01-06-2023', 'dd-mm-yyyy'), to_date('06-11-2023', 'dd-mm-yyyy'));
+create or replace procedure printMonthlyWateringTotal(parcelaId parcela.id%type,
+                                                    beginDate operacao_agricola.data%type,
+                                                    endDate operacao_agricola.data%type)
+    is
+    c1          sys_refcursor := getMonthlyWateringTotal(parcelaId, beginDate, endDate);
+    mes         number(2);
+    totalRega   number(10);
+    parcelaNome parcela.nome%type;
+begin
+    select nome
+    into parcelaNome
+    from parcela
+    where parcela.id = parcelaId;
+
+    dbms_output.put_line('Parcela: ' || parcelaNome);
+    dbms_output.put_line('-----------------------------');
+
+    loop
+        fetch c1 into mes, totalRega;
+        dbms_output.put_line('Mes: ' || mes);
+        dbms_output.put_line('Total de rega: ' || mes);
+        dbms_output.put_line('-----------------------------');
+        exit when c1%notfound;
+    end loop;
+end;
+
+begin
+    printMonthlyWateringTotal(108, to_date('01-06-2023', 'dd-mm-yyyy'), to_date('06-11-2023', 'dd-mm-yyyy'));
 end;
