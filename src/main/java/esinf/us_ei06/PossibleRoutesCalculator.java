cb7bf80a1015a30ab8e6b3d9f97744fff2c742de
1221638 <1221638@isep.ipp.pt>
2023-12-19 13:44:25 +0000

    L3IP-129 #time 5m

--- src/main/java/esinf/us_ei06/PossibleRoutesCalculator.java
+++ src/main/java/esinf/us_ei06/PossibleRoutesCalculator.java
@@ -1,34 +1,52 @@
 package esinf.us_ei06;
 
 import esinf.Graph;
+import esinf.IntegerBinaryOperator;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.function.BinaryOperator;
 
 public class PossibleRoutesCalculator {
 
-    public static <V, E> void allPaths(Graph<V, E> graph, V origin, V destiny, boolean[] visited,
-                                        LinkedList<V> path, ArrayList<LinkedList<V>> paths) {
-        int vKey = graph.key(origin);
-        if (visited[vKey]) return;
+    public static <V, E> void getAllPossibleRoutes(Graph<V, E> graph,
+                                                   V origin,
+                                                   V destiny,
+                                                   E autonomy,
+                                                   E distanceTravelled,
+                                                   BinaryOperator<E> operat,
+                                                   Comparator<E> comparat,
+                                                   boolean[] visited,
+                                                   LinkedList<V> route,
+                                                   ArrayList<LinkedList<V>> routes) {
+        int origKey = graph.key(origin);
+        if (visited[origKey]) return;
 
-        if (origin.equals(destiny)) {
-            LinkedList<V> pathCopy = new LinkedList<>(path);
-            pathCopy.addFirst(destiny);
-            Collections.reverse(pathCopy);
-            paths.add(new LinkedList<>(pathCopy));
-            return;
-        }
+        E distance=graph.edge(route.peekFirst(), origin).getWeight();
+        distanceTravelled = operat.apply(distanceTravelled, distance);
 
-        path.push(origin);
-        visited[vKey] = true;
+        if (comparat.compare(distanceTravelled, autonomy) < 0){
 
-        for (V vAdj : graph.adjVertices(origin)) {
-            allPaths(graph, vAdj, destiny, visited, path, paths);
-        }
-        path.pop();
-        visited[vKey] = false;
+            if (origin.equals(destiny)) {
+                LinkedList<V> pathCopy = new LinkedList<>(route);
+                pathCopy.addFirst(destiny);
+                Collections.reverse(pathCopy);
+                routes.add(new LinkedList<>(pathCopy));
+                return;
+            }
 
+            route.push(origin);
+            visited[origKey] = true;
+
+            for (V vAdj : graph.adjVertices(origin)) {
+                getAllPossibleRoutes(graph, vAdj, destiny, autonomy, distanceTravelled, operat, comparat, visited, route, routes);
+            }
+
+        } else return;
+
+        route.pop();
+        visited[origKey] = false;
     }
 }
