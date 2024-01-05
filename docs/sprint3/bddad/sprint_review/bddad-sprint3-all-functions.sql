88af445ade215c88574280d32eb841cdad9372bb
1221638 <1221638@isep.ipp.pt>
2024-01-05 13:47:37 +0000

    preparação dos scripts para apresentação de bddad, sprint 3

--- docs/sprint3/bddad/sprint_review/bddad-sprint3-all-functions.sql
+++ docs/sprint3/bddad/sprint_review/bddad-sprint3-all-functions.sql
@@ -2063,3 +2063,23 @@ BEGIN
     commit;
     return true;
 END;
+
+
+
+
+
+
+
+CREATE OR REPLACE TRIGGER REGA_INSERT_TRIGGER
+    AFTER INSERT ON Rega
+    FOR EACH ROW
+DECLARE
+    v_log_message NVARCHAR2(255);
+BEGIN
+    -- Customize the log message as needed
+    v_log_message := 'Nova entrada de Rega com operacao_id: ' || :NEW.operacao_id || ', setor_id: ' || :NEW.setor_id || ', duracao: ' || :NEW.duracao || ', hora: ' || :NEW.hora;
+
+    -- Insert into LOG_OPERACAO table
+    INSERT INTO LOG_OPERACAO (id, operacao_id, log)
+    VALUES (log_operacao_seq.NEXTVAL, :NEW.operacao_id, v_log_message);
+END;
